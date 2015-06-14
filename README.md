0a8ef020d35fba2eae333e98c268a915efe1aff2
albertocavia <acaviaz@moradas.es>
2015-06-14 16:45:59 +0200

    PRACTICA P2P MOD 5

---
+++ README.md
@@ -0,0 +1,2 @@
+ACZ 2015: Java script que devuelve número aleatorio entre 0 y 1.
+14/06/2015 
--- random.js
+++ random.js
@@ -1,6 +1,7 @@
 // Math.random() devuelve número aleatorio entre 0 y 1.
 var numero = Math.random();
 
+console.log('\n ACZ 2015: Java script que devuelve número aleatorio entre 0 y 1. \n' )
 var str =' MAYOR que 0,5';
 
 if (numero <= 0.5){
